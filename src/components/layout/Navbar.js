5ecede591dc5dc8c66378a1d282208fcfea3c76d
CarlJstn <carljustinedg@gmail.com>
2019-08-18 10:16:50 +0800

    firebase login

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import SignedInLinks from './SignedInLinks';
-import SignedOutLinks from './SignedOutLinks';
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
+import { connect } from "react-redux";
 
 const Navbar = () => {
   return (
-    <nav className='nav-wrapper grey darken-3'>
-      <div className='container'>
-        <Link to='/' className='brand-logo'>
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to="/" className="brand-logo">
           ProjectsArchive
         </Link>
         <SignedInLinks />
@@ -18,4 +19,10 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => {
+  console.log(state);
+
+  return {};
+};
+
+export default connect(mapStateToProps)(Navbar);
