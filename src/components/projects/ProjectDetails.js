4845df4da4a77e71d157fbfa0371df1954788810
CarlJstn <carljustinedg@gmail.com>
2019-08-18 09:18:28 +0800

    individual view of project

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
-class ProjectDetails extends Component {
-   state = {
-    project: {}
-  }
+const ProjectDetails = props => {
+  const { project } = props;
 
-componentWilMount( ){
-  
-}
-
-
-  render() {
-    const id = this.props.match.params.id;
-    console.log("ad",this.props);
-
+  if (project) {
     return (
-      <div className='container section project-details'>
-        <div className='card z-depth-0'>
-          <div className='card-content'>
-            <span className='card-title'>Project Title - {id}</span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto tempora impedit cumque voluptate aliquam corrupti sint voluptas repudiandae. At sunt provident velit consequatur accusamus, a praesentium unde reprehenderit aliquid ea.</p>
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
           </div>
-          <div className='card-action grey lighten-4 grey-text'>
-            <div>Posted by The Net Ninja</div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>
+              Posted by {project.authorFirstName} {project.authorLastName}
+            </div>
             <div>16th August, 10:43am</div>
           </div>
         </div>
       </div>
     );
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
+    );
   }
-}
+};
 
 const mapStateToProps = (state, ownProps) => {
-  const id = ownProps.match.params.id;
-  const projects = state.firestoreConnect;
-  const project = projects ? projects[id] : null;
+  let project = {};
+
+  if (state.firestore.data.projects) {
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    project = projects ? projects[id] : null;
+  }
   return {
     project: project
   };
@@ -47,7 +49,7 @@ export default compose(
   connect(mapStateToProps),
   firestoreConnect([
     {
-      collection: 'projects'
+      collection: "projects"
     }
   ])
 )(ProjectDetails);
